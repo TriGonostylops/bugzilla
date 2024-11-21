383a9ab1e564df5f15369dfb710bb9e886e2b2e3
Balázsi Márton <h975393@stud.u-szeged.hu>
2024-11-21 22:15:02 +0100

    feat: login, logout, basic navbar

--- bugzilla/public/index.php
+++ bugzilla/public/index.php
@@ -1,33 +1,65 @@
 <?php
 
-// Autoloading controllers (make sure to include all necessary files)
+session_start();
+include '../views/navbar.php';
 require_once '../controllers/IndexController.php';
 require_once '../controllers/UserController.php';
 
-// Get the action from the query string (or default to 'index')
+// BaseController class to manage common initialization
+abstract class BaseController
+{
+    protected $userController;
+    protected $indexController;
+
+    public function __construct()
+    {
+        $this->userController = new UserController();
+        $this->indexController = new IndexController();
+    }
+}
+
+// FrontController for routing
+class FrontController extends BaseController
+{
+    public function route($action)
+    {
+        switch ($action) {
+            case 'index':
+                $this->indexController->index();
+                break;
+
+            case 'register':
+                $this->userController->register();
+                break;
+
+            case 'login':
+                $this->userController->login();
+                break;
+            case 'logout':
+                $this->userController->logout();
+                break;
+            default:
+                $this->show404();
+                break;
+        }
+    }
+
+    private function isSessionActive()
+    {
+        var_dump($_SESSION);
+        return isset($_SESSION['user']);
+    }
+
+    private function show404()
+    {
+        echo "404 - Action not found.";
+    }
+}
+
+// Fetch the requested action or default to 'index'
 $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : 'index';
 
-// Instantiate the appropriate controller based on the action
-switch ($action) {
-    case 'index':
-        // Instantiate the Index controller and call the index method
-        $controller = new IndexController();
-        $controller->index();
-        break;
+// Instantiate the front controller and route the request
+$frontController = new FrontController();
+$frontController->route($action);
 
-    case 'register':
-        // Instantiate the User controller and call the register method
-        $controller = new UserController();
-        $controller->register();
-        break;
-
-    case 'login':
-        // Instantiate the User controller and call the login method
-        $controller = new UserController();
-        $controller->login();
-        break;
-
-    default:
-        echo "404 - Action not found.";
-        break;
-}
